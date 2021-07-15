12e30e9e03bca74cb751c0e1c6455e470c119c52
unknown <jiadongwangbuaa@163.com>
2021-07-15 11:37:11 +0800

    i

--- src/pipe.py
+++ src/pipe.py
@@ -5,13 +5,22 @@ read=load_component('../components/read.yaml')
 trans=load_component('../components/trans.yaml')
 tag=load_component('../components/tag.yaml')
 @dsl.pipeline(name='try')
-def mytry():
+def mytry(translate):
 	a=read()
-	t=trans(a.output)
-	k=tag(t.output)
+	with dsl.Condition(translate==True):
+		t=trans(a.output)
+		k=tag(t.output)
+	with dsl.Condition(translate==False):
+		k=tag(a.output)
 	return
 kfp.compiler.Compiler().compile(mytry, '../pipeline/mytry.yaml')
-# client.create_run_from_pipeline_func(
-#     mytry,
-#     arguments={},
-#     )
+
+client = kfp.Client(host='http://ml-pipeline.ingress.isa.buaanlsde.cn/')
+client.create_run_from_pipeline_func(
+    mytry,
+    arguments={'translate':True},
+    )
+client.create_run_from_pipeline_func(
+    mytry,
+    arguments={'translate':False},
+    )
